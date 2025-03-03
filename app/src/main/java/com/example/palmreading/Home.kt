96a87b7f71568bdb629326a2df8f4bd4ed4a9af1
QuangNHApero <QuangNH@apero.vn>
2025-03-03 22:42:08 +0700

    setting

--- app/src/main/java/com/example/palmreading/Home.kt
+++ app/src/main/java/com/example/palmreading/Home.kt
@@ -11,14 +11,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -26,7 +24,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.paint
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -47,20 +44,24 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(top = 60.pxToDp(), start = 24.pxToDp(),end = 24.pxToDp()),
+                .padding(top = 60.pxToDp(), start = 24.pxToDp(), end = 24.pxToDp()),
             verticalArrangement = Arrangement.spacedBy(24.pxToDp()),
         ) {
-            HomeHeader(modifier = Modifier
-            .width(380.pxToDp())
-            .height(80.pxToDp()))
+            HomeHeader(
+                modifier = Modifier
+                    .width(380.pxToDp())
+                    .height(80.pxToDp())
+            )
 
             Column(
                 verticalArrangement = Arrangement.spacedBy(24.pxToDp())
-            ){
+            ) {
                 palmReadingItems.forEach { item ->
-                    HomeCard(item, modifier = Modifier
-                        .width(382.pxToDp())
-                        .height(156.pxToDp()))
+                    HomeCard(
+                        item, modifier = Modifier
+                            .width(382.pxToDp())
+                            .height(156.pxToDp())
+                    )
                 }
             }
         }
@@ -99,12 +100,12 @@ fun GradientBackgroundWithImage(content: @Composable () -> Unit) {
 
 
 @Composable
-fun HomeHeader(modifier: Modifier = Modifier, text : String = "Palm Reading App") {
+fun HomeHeader(modifier: Modifier = Modifier, text: String = "Palm Reading App") {
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(16.pxToDp())
-    ){
+    ) {
         Image(
             painter = painterResource(R.drawable.ic_home_logo),
             contentDescription = null,
@@ -121,23 +122,25 @@ fun HomeHeader(modifier: Modifier = Modifier, text : String = "Palm Reading App"
                 .weight(1f)
         )
 
-        SettingButton { /*ToDo: handle setting button*/ }
+        SettingButton { /*TODO*/ }
 
     }
 }
 
 
 @Composable
-fun SettingButton(onClick: ()-> Unit){
-    IconButton(onClick) {
+fun SettingButton(onSettingClick: () -> Unit) {
+    IconButton(onSettingClick) {
         Image(
             painter = painterResource(R.drawable.ic_home_setting),
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .size(24.pxToDp())
         )
     }
 }
+
 
 @Composable
 fun HomeCard(item: HomeCardItem, modifier: Modifier = Modifier) {
@@ -146,56 +149,60 @@ fun HomeCard(item: HomeCardItem, modifier: Modifier = Modifier) {
         border = BorderStroke(width = 2.pxToDp(), Color(0xFFA17B58)),
         colors = CardDefaults.cardColors(containerColor = Color(0xFF4A0A82).copy(alpha = 0.8f)),
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .shadow(
-                elevation = 8.pxToDp(),
-                shape = RoundedCornerShape(16.pxToDp())
+                elevation = 20.pxToDp(),
+                shape = RoundedCornerShape(24.pxToDp()),
+                spotColor = Color(0xFFF17D12)
             )
             .clickable { item.onCardClick() }
     ) {
-        Row(
-            modifier = Modifier.padding(16.pxToDp()),
-            verticalAlignment = Alignment.CenterVertically
-        ){
-            Image(
-                painter = painterResource(item.imageResId),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(120.pxToDp())
-                    .clip(RoundedCornerShape(16.pxToDp()))
-            )
-            Spacer(modifier = Modifier.width(12.pxToDp()))
-            Column(
-                modifier = Modifier.weight(1f)
-            ) {
-                Text(
-                    text = item.title,
-                    fontSize = 18f.pxToSp(),
-                    fontWeight = FontWeight.Bold,
-                    fontFamily = boldFont,
-                    overflow = TextOverflow.Ellipsis,
-                    color = Color.White
-                )
-                Text(
-                    text = item.description,
-                    fontSize = 12f.pxToSp(),
-                    fontFamily = mediumFont,
-                    overflow = TextOverflow.Ellipsis,
-                    color = Color(0xFF848890)
-                )
-            }
-        }
+        CardContent(item)
     }
 }
 
 
-
+@Composable
+fun CardContent(item: HomeCardItem, modifier: Modifier = Modifier){
+    Row(
+        modifier = Modifier.padding(16.pxToDp()),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            painter = painterResource(item.imageResId),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(120.pxToDp())
+                .clip(RoundedCornerShape(16.pxToDp()))
+        )
+        Spacer(modifier = Modifier.width(12.pxToDp()))
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = item.title,
+                fontSize = 18f.pxToSp(),
+                fontWeight = FontWeight.Bold,
+                fontFamily = boldFont,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.White
+            )
+            Text(
+                text = item.description,
+                fontSize = 12f.pxToSp(),
+                fontFamily = mediumFont,
+                overflow = TextOverflow.Ellipsis,
+                color = Color(0xFF848890)
+            )
+        }
+    }
+}
 
 data class HomeCardItem(
     val title: String,
     val description: String,
-    val imageResId : Int,
+    val imageResId: Int,
     val onCardClick: () -> Unit
 )
 
@@ -220,7 +227,7 @@ val palmReadingItems = listOf(
     )
 )
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 430)
 @Composable
 fun HomeShow() {
     PalmReadingTheme {
